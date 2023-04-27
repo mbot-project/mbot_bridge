34f5f9417b15cf5d657349e223c5c13f81e10a0f
Jana <jbpavlasek@gmail.com>
2023-04-27 12:05:09 -0400

    Use getattr to convert strings to LCM types so we don't have to manually import each type

--- src/mbot_bridge/utils/type_utils.py
+++ src/mbot_bridge/utils/type_utils.py
@@ -1,19 +1,27 @@
-from mbot_lcm_msgs import pose2D_t
-
-LCM_TYPES = {
-    "pose2D_t": pose2D_t
-}
+import mbot_lcm_msgs
 
 
-def decode(data, type_name):
+def str_to_lcm_type(dtype):
+    """Accesses the message type by string. Raises AttributeError if the class type is invalid."""
+    return getattr(mbot_lcm_msgs, dtype)
+
+
+def decode(data, dtype):
     """Decode raw data from LCM channel to type based on type string."""
-    if type_name not in LCM_TYPES:
-        raise Exception(f"LCM Type {type_name} is not supported.")
-
-    return LCM_TYPES[type_name].decode(data)
+    lcm_obj = str_to_lcm_type(dtype)
+    return lcm_obj.decode(data)
 
 
 def lcm_type_to_dict(data):
     """LCM types, once decoded"""
     data_d = {att: getattr(data, att) for att in data.__slots__}
     return data_d
+
+
+def dict_to_lcm_type(data, dtype):
+    lcm_msg = str_to_lcm_type(dtype)()
+
+    for k, v in data.items():
+        setattr(lcm_msg, k, v)
+
+    return lcm_msg
