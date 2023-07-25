2ded39fd2e47114048257bceb2ae26a728ac8cb7
Jana Pavlasek <jbpavlasek@gmail.com>
2023-07-25 16:40:34 -0400

    Add rule to build C++ code in setup

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 # Third Party
+import os
+import subprocess
 import setuptools
+from setuptools.command.build_ext import build_ext
 
-setuptools.setup()
+
+class CMakeBuild(build_ext):
+    def run(self):
+        # Make sure that CMake is installed
+        if not os.path.exists('build'):
+            os.makedirs('build')
+        subprocess.check_call(['cmake', '../mbot_cpp'], cwd='build')
+        subprocess.check_call(['make'], cwd='build')
+
+
+setuptools.setup(
+    cmdclass={'build_ext': CMakeBuild},
+)
