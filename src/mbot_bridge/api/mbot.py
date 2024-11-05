ed75c952e8983248a2ba12cb9ccb843643490f7f
Jana Pavlasek <jbpavlasek@gmail.com>
2024-11-05 14:08:48 -0500

    Add functionality to return requested data as bytes, add documentation

--- src/mbot_bridge/api/mbot.py
+++ src/mbot_bridge/api/mbot.py
@@ -44,8 +44,22 @@ class MBot(object):
 
     """SUBSCRIBERS"""
 
-    async def _request(self, ch, dtype=None, as_bytes=False):
-        res = MBotJSONRequest(ch, dtype=dtype, as_bytes=as_bytes)
+    async def _request(self, ch, dtype=None, as_bytes=False, request_as_bytes=False):
+        """Internal wrapper to request data from the MBot Bridge Server.
+
+        Args:
+            ch (str): The channel to request data from.
+            dtype (str, optional): The LCM data type to expect from the data. Must be provided if as_bytes is False.
+                                   Defaults to None.
+            as_bytes (bool, optional): Whether to return the data as bytes. Defaults to False.
+            request_as_bytes (bool, optional): Whether to request the data to be sent as bytes. Defaults to True.
+
+        Returns:
+            (bytes or obj): The data returned by the server. If as_bytes is False, the data is returned as an
+                            LCM message type. If True, the data is returned in raw bytes. Returns None if fetching the
+                            data fails.
+        """
+        res = MBotJSONRequest(ch, dtype=dtype, as_bytes=request_as_bytes)
         try:
             async with websockets.connect(self.uri, open_timeout=self.connect_timeout) as websocket:
                 await websocket.send(res.encode())
@@ -56,8 +70,15 @@ class MBot(object):
             print(f"[MBot API] ERROR: Cannot connect to MBot Bridge at: {self.uri}")
             return
 
+        # If the data was requested as bytes and returned as bytes, return the raw data.
+        if isinstance(response, bytes) and as_bytes:
+            return response
+
+        # If the data was not requested as bytes, try to decode it.
         if isinstance(response, bytes):
-            assert dtype is not None, "Must provide data type to process data as bytes."
+            if dtype is None:
+                print("[MBot API] ERROR: Must provide data type to process data as bytes.")
+                return
             try:
                 msg = type_utils.decode(response, dtype)
             except type_utils.BadMessageError as e:
@@ -65,7 +86,8 @@ class MBot(object):
                 return
 
             return msg
-        # Convert this to a JSON message.
+
+        # If this was not bytes, process the JSON message.
         response = MBotJSONMessage(response, from_json=True)
 
         # Check if this is an error. If so, print it and quit.
@@ -97,7 +119,7 @@ class MBot(object):
     def read_odometry(self):
         res = asyncio.run(self._request(self.lcm_config.ODOMETRY.channel,
                                         self.lcm_config.ODOMETRY.dtype,
-                                        as_bytes=True))
+                                        as_bytes=False, request_as_bytes=True))
         if res is not None:
             return [res.x, res.y, res.theta]
 
@@ -106,7 +128,7 @@ class MBot(object):
     def read_slam_pose(self):
         res = asyncio.run(self._request(self.lcm_config.SLAM_POSE.channel,
                                         self.lcm_config.SLAM_POSE.dtype,
-                                        as_bytes=True))
+                                        as_bytes=False, request_as_bytes=True))
         if res is not None:
             return [res.x, res.y, res.theta]
 
@@ -115,12 +137,25 @@ class MBot(object):
     def read_lidar(self):
         res = asyncio.run(self._request(self.lcm_config.LIDAR.channel,
                                         self.lcm_config.LIDAR.dtype,
-                                        as_bytes=True))
+                                        as_bytes=False, request_as_bytes=True))
         if res is not None:
             return res.ranges, res.thetas
 
         return [], []
 
-    def read_data(self, channel, dtype):
-        res = asyncio.run(self._request(channel, dtype, as_bytes=True))
+    def read_data(self, channel, dtype=None, as_bytes=False):
+        """Reads the latest data on a given channel and returns it.
+
+        Args:
+            channel (str): The name of the channel to read the data from.
+            dtype (str, optional): The data type of the data on the channel. Required if as_bytes is False. Defaults to None.
+            as_bytes (bool, optional): Whether to return the data as raw bytes. If False, the data will be decoded as
+                                       the dtype. Defaults to False.
+
+        Returns:
+            bytes or obj: The data returned by the server. If as_bytes is False, the data is returned as an
+                          LCM message type. If True, the data is returned in raw bytes. Returns None if fetching the
+                          data fails.
+        """
+        res = asyncio.run(self._request(channel, dtype, as_bytes=as_bytes, request_as_bytes=as_bytes))
         return res
